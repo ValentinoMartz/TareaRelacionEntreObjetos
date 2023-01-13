54ba1985054223303dab24cfa5f5b4a021b07dfa
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-13 13:12:54 -0300

    first commit

---
+++ LibRelacionesPOO/Caso 1/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibRelacionesPOO.Caso_1
+{
+    public class Cliente
+    {
+        public Cliente(string nombre, string password, Usuario usuario)
+        {
+            Nombre = nombre;
+            Password = password;
+            Usuario = usuario;
+        }
+
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ LibRelacionesPOO/Caso 1/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibRelacionesPOO.Caso_1
+{
+    public class Usuario
+    {
+        public Usuario(string nombre, string password, Cliente cliente)
+        {
+            Nombre = nombre;
+            Password = password;
+            Cliente = cliente;
+        }
+
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
